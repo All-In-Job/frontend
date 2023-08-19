70a1a9e2df0c7a389a482412c826333209ac3eef
chlgusrb97 <ikolp34@gmail.com>
2023-08-19 11:56:38 +0900

    design: 디자인 수정

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,12 +1,5 @@
-import PostCard from "../commons/PostCard/PostCard";
 import * as S from "./CardList.style";
 
 export const CardList = () => {
-  return (
-    <S.Section>
-      {[1, 2, 3, 4].map((_) => (
-        <PostCard />
-      ))}
-    </S.Section>
-  );
+  return <S.Section>CardList</S.Section>;
 };
