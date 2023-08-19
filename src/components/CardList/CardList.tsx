d3d0330d6c6b5add0dc3e5545c2382db073d352f
chlgusrb97 <ikolp34@gmail.com>
2023-08-19 10:10:10 +0900

    design: remove 컴포넌트

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,11 +1,5 @@
-import InterestFieldSetup from "../InterestFieldSetup/InterestFieldSetup";
 import * as S from "./CardList.style";
 
 export const CardList = () => {
-  return (
-    <S.Section>
-      CardList
-      <InterestFieldSetup />
-    </S.Section>
-  );
+  return <S.Section>CardList</S.Section>;
 };
