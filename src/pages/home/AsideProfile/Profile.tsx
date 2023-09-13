86db1f03f02dce3f29b239d6d89f3f8490d61a2a
박정훈 <fireking5997@famoz.co.kr>
2023-09-13 19:28:38 +0900

    chore:프로파일 첫 단락 작성

--- src/pages/home/AsideProfile/Profile.tsx
+++ src/pages/home/AsideProfile/Profile.tsx
@@ -10,8 +10,10 @@ const Profile = () => {
       <UserInfo>
         <Avatar />
         <UserDescContainer>
-          <Desc size='25px'>{`${user.name}님`}</Desc>
-          <Desc size='25px'>{`열정온도${user.temperature}℃`}</Desc>
+          <MarginBottomDesc size='15px'>{`${user.name}님`}</MarginBottomDesc>
+          <FlexDesc size='15px'>
+            열정온도<YellowDesc size='15px'>{`${user.temperature}℃`}</YellowDesc>
+          </FlexDesc>
         </UserDescContainer>
         <LogoutButton>로그아웃</LogoutButton>
       </UserInfo>
@@ -22,8 +24,51 @@ const Profile = () => {
 
 export default Profile;
 
-const Container = styled.div``;
-const UserInfo = styled.div``;
-const Avatar = styled.div``;
-const UserDescContainer = styled.div``;
-const LogoutButton = styled.div``;
+const Container = styled.div`
+  padding: 15px 19px;
+  border-radius: 12px;
+  background: var(--orange-100, #ffe8df);
+`;
+const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const Avatar = styled.div`
+  width: 58px;
+  height: 58px;
+  border-radius: 50%;
+  border: 2px solid var(--orange-500, #fd6b36);
+`;
+const UserDescContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 11.5px;
+  font-weight: 700;
+`;
+const LogoutButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 30px;
+  width: 54px;
+  height: 24px;
+  color: var(--black-200, #a0a09f);
+  cursor: pointer;
+  font-size: 12px;
+  border-radius: 4px;
+  background: var(--background-primary, #ededed);
+`;
+
+const MarginBottomDesc = styled(Desc)`
+  margin-bottom: 6px;
+`;
+
+const FlexDesc = styled(Desc)`
+  display: flex;
+`;
+
+const YellowDesc = styled(Desc)`
+  margin-left: 4px;
+  color: #fd6b36;
+`;
