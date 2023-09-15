216b1862f7c2a067b60756aa04e247f882eecb14
chlgusrb97 <ikolp34@gmail.com>
2023-09-16 01:55:24 +0900

    feat + design: 관심분야 설정 디자인 변경 및 전공학과 Input 란 동적 스타일링

--- src/components/InterestFieldSetup/InterestFieldSetup.tsx
+++ src/components/InterestFieldSetup/InterestFieldSetup.tsx
@@ -3,85 +3,91 @@ import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import Submit from 'components/commons/Buttons/Submit/Submit';
 
 import * as S from './InterestFieldSetup.styles';
+import { ReactComponent as DefaultInterestImage } from './res/img/default_interest_image.svg';
 
 const interestTags = [
   {
-    name: '#문학',
-    icon: '',
-    image: '',
-    keyWords: ['#시', '#소설', '#논문', '#논술', '#만화'],
-  },
-  {
-    name: '#디자인',
-    icon: '',
-    image: '',
-    keyWords: ['#시각디자인', '#제품디자인', '#환경디자인'],
-  },
-  {
-    name: '#IT, 프로그래밍',
-    icon: '',
-    image: '',
+    name: '공모전',
     keyWords: [
-      '#웹 개발',
-      '#응용프로그램 개발',
-      '#ERP시스템 개발/설계',
-      '#네트워크/서버/보안',
-      '#DBA/데이터베이스',
-      '#콘텐츠/사이트운영',
-      '#기획/PM',
-      '#HTML/퍼블리싱/UI개발',
-      '#게임',
+      '기획/아이디어',
+      '광고/마케팅',
+      '사진/영상/UCC',
+      '디자인/순수미술/공예',
+      '네이밍/슬로건',
+      '캐릭터/만화/게임',
+      '건축/건설/인테리어',
+      '과학/공학',
+      '예체능/패션',
+      '전시/페스티벌',
+      '문학/시나리오',
+      '해외',
+      '학술',
+      '창업',
+      '기타',
     ],
   },
   {
-    name: '#금융',
-    icon: '',
-    image: '',
-    keyWords: ['#은행', '#증권', '#보험'],
-  },
-  {
-    name: '#예체능',
-    icon: '',
-    image: '',
-    keyWords: ['#체육', '#미술', '#음악', '#무용', '#연극영화'],
-  },
-  {
-    name: '#어학',
-    icon: '',
-    image: '',
+    name: '대외활동',
     keyWords: [
-      '#영어',
-      '#중국어',
-      '#스페인어',
-      '#프랑스어',
-      '#이탈리아어',
-      '#포르투갈어',
-      '#러시아어',
-      '#일본어',
+      '여행/호텔/항공',
+      '언론/미디어',
+      '문화/역사',
+      '행사/페스티벌',
+      '교육',
+      '디자인/사진/예술/영상',
+      '경제/금융',
+      '경영/컨설팅/마케팅',
+      '정치/사회/법률',
+      '체육/헬스',
+      '의료/보건',
+      '뷰티/미용/화장품',
+      '과학/공학/기술/IT',
+      '요리/식품',
+      '창업/자기계발',
+      '환경/에너지',
+      '콘텐츠',
+      '사회공헌/교류',
+      '유통/물류',
+      '기타',
     ],
   },
   {
-    name: '#창업, 사업',
-    icon: '',
-    image: '',
-    keyWords: ['#스타트업', '#서비스', '#제조', '#프랜차이즈', '#사회적기업', '#투자'],
+    name: '자격증',
+    keyWords: [
+      '경영/회계/사무',
+      '보건/의료',
+      '문화예술/디자인/방송',
+      '사회복지/종교',
+      '이용/숙박/여행/오락/스포츠',
+      '음식서비스',
+      '기계',
+      '정보통신',
+      '식품/가공',
+      '환경/에너지',
+    ],
   },
   {
-    name: '#취업',
-    icon: '',
-    image: '',
+    name: '어학',
+    keyWords: ['영어', '중국어', '스페인어', '일본어', '포르투갈어', '아랍어'],
+  },
+  {
+    name: '인턴',
     keyWords: [
-      '#영업/고객상담',
-      '#경영/사무',
-      '#마케팅/광고/홍보',
-      '#생산/제조',
-      '#연구개발/설계',
-      '#서비스',
-      '#무역유통',
-      '#의료',
-      '#건설',
-      '#교육',
-      '#전문/특수직',
+      '영업/고객상담',
+      '경영/사무',
+      '마케팅/광고/홍보',
+      '생산/제조',
+      '연구개발/설계',
+      'IT/인터넷',
+      '서비스',
+      '무역/유통',
+      '의료',
+      '건설',
+      '교육',
+      '디자인',
+      '전문/특수직',
+      '미디어',
+      '기타',
     ],
   },
 ];
@@ -111,18 +117,19 @@ const departments = [
 
 type InterestTag = {
   name: string;
-  image: string;
   keyWords: string[];
 };
 
 function InterestFieldSetup() {
   const [departmentInput, setDepartmentInput] = useState('');
   const [choiceDepartment, setChoiceDepartment] = useState('');
+
   const inputRef = useRef<HTMLInputElement>(null);
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
   const [interestTag, setInterestTag] = useState<InterestTag>({
-    name: '',
-    image: '',
-    keyWords: [],
+    name: interestTags[0].name,
+    keyWords: interestTags[0].keyWords,
   });
   const [selectedKeyWords, setSelectedKeyWords] = useState<string[]>([]);
   const [isVisible, setIsVisible] = useState(false);
@@ -135,7 +142,16 @@ function InterestFieldSetup() {
       setIsVisible(true);
     } else {
       setIsVisible(false);
+      setChoiceDepartment('');
     }
+  };
+
+  const handleInputFocus = () => {
+    setIsInputFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setIsInputFocused(false);
   };
 
   const searchedDepartment = departments.filter(department => department.includes(departmentInput));
@@ -152,7 +168,6 @@ function InterestFieldSetup() {
   const onClickTag = (tag: InterestTag) => {
     setInterestTag({
       name: tag.name,
-      image: tag.image,
       keyWords: tag.keyWords,
     });
 
@@ -172,8 +187,8 @@ function InterestFieldSetup() {
   };
 
   useEffect(() => {
-    setIsActive(selectedKeyWords.length > 0);
-  }, [selectedKeyWords]);
+    setIsActive(choiceDepartment !== '' && selectedKeyWords.length > 0);
+  }, [choiceDepartment, selectedKeyWords]);
 
   const submitButton = () => {
     console.log({
@@ -185,13 +200,24 @@ function InterestFieldSetup() {
 
   return (
     <S.InterestFieldSetupWrapper>
-      <S.MajorDepartment>
-        <S.InterestFieldSetupTitle>전공학과를 선택해주세요!</S.InterestFieldSetupTitle>
+      <S.InterestFieldSetupTitle>전공학과를 선택해주세요!</S.InterestFieldSetupTitle>
+      <S.MajorDepartment
+        choiceDepartment={choiceDepartment}
+        isVisible={isVisible}
+        isInputFocused={isInputFocused}
+      >
         <input
           type='text'
-          placeholder='학과를 입력해주세요.'
+          placeholder='전공학과를 입력해주세요.'
           onChange={getValueDepartmentInput}
           ref={inputRef}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+        />
+        <S.ExpandMoreIcon
+          choiceDepartment={choiceDepartment}
+          data-isvisible={isVisible}
+          data-isinputfocused={isInputFocused}
         />
         {isVisible && (
           <S.MajorDepartmentList>
@@ -204,57 +230,54 @@ function InterestFieldSetup() {
         )}
       </S.MajorDepartment>
 
-      <S.InterestSelect>
-        <S.InterestFieldSetupTitle>관심 분야를 선택해주세요!</S.InterestFieldSetupTitle>
-        <S.TagList>
-          {interestTags.map(tag => (
-            <S.ClickedTag
-              key={tag.name}
-              onClick={() =>
-                onClickTag({
-                  name: tag.name,
-                  image: tag.image,
-                  keyWords: [...tag.keyWords],
-                })
-              }
-              isChangeColor={tag.name === interestTag.name ? true : false}
-            >
-              <S.TagName>{tag.name}</S.TagName>
-              <S.TagIcon src={tag.icon} />
-            </S.ClickedTag>
-          ))}
-        </S.TagList>
-      </S.InterestSelect>
+      <S.DefaultImageBox>
+        <DefaultInterestImage />
+      </S.DefaultImageBox>
 
-      {interestTag.name !== '' && (
-        <>
-          <S.InterestKeyWord>
-            <S.InterestFieldSetupTitle>
-              <strong>{interestTag.name} </strong>분야의 관심 키워드를 선택해주세요!
-            </S.InterestFieldSetupTitle>
-            <S.KeyWordText>키워드는 1개~3개 선택 가능합니다.</S.KeyWordText>
-            <S.TagImage src={interestTag.image} />
-            <S.KeyWordList>
-              {interestTag.keyWords.map(keyWord => (
-                <S.ClickedKeyWord
-                  key={keyWord}
-                  onClick={() => selectedKeyWord(keyWord)}
-                  isChangeColor={selectedKeyWords.includes(keyWord)}
-                >
-                  <S.CheckBox isChangeColor={selectedKeyWords.includes(keyWord)}></S.CheckBox>
-                  <p>{keyWord}</p>
-                </S.ClickedKeyWord>
-              ))}
-            </S.KeyWordList>
-          </S.InterestKeyWord>
-          <Submit
-            title='확인'
-            onClick={submitButton}
-            disabled={isActive ? false : true}
-            isActive={isActive}
-          />
-        </>
-      )}
+      <S.InterestFieldSetupTitle>관심분야를 선택해주세요!</S.InterestFieldSetupTitle>
+      <S.InterestSelectTagList>
+        {interestTags.map(tag => (
+          <S.ClickedTag
+            key={tag.name}
+            onClick={() =>
+              onClickTag({
+                name: tag.name,
+                keyWords: [...tag.keyWords],
+              })
+            }
+            isChangeColor={tag.name === interestTag.name ? true : false}
+          >
+            {tag.name}
+          </S.ClickedTag>
+        ))}
+      </S.InterestSelectTagList>
+
+      <S.InterestKeyWord>
+        <S.InterestFieldSetupTitle>
+          #{interestTag.name} 분야의 관심 키워드를 선택해주세요!
+        </S.InterestFieldSetupTitle>
+        <S.KeyWordText>키워드는 1개~3개 선택 가능합니다.</S.KeyWordText>
+
+        <S.KeyWordList>
+          {interestTag.keyWords.map(keyWord => (
+            <S.ClickedKeyWord
+              key={keyWord}
+              onClick={() => selectedKeyWord(keyWord)}
+              isChangeColor={selectedKeyWords.includes(keyWord)}
+            >
+              {/* <S.CheckBox isChangeColor={selectedKeyWords.includes(keyWord)}></S.CheckBox> */}
+              <S.CheckCircleIcon data-isactive={selectedKeyWords.includes(keyWord)} />
+              <p>#{keyWord}</p>
+            </S.ClickedKeyWord>
+          ))}
+        </S.KeyWordList>
+      </S.InterestKeyWord>
+      <Submit
+        title='확인'
+        onClick={submitButton}
+        disabled={isActive ? false : true}
+        isActive={isActive}
+      />
     </S.InterestFieldSetupWrapper>
   );
 }
