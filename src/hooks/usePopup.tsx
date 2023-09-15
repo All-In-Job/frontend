0b3b606e49c019fbfec110716359e34671254e0a
fireking5997@gmail.com <fireking5997@gmail.com>
2023-09-15 21:22:15 +0900

    chore:popup 여닫는 공통사용 훅 추가

---
+++ src/hooks/usePopup.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef, useState } from 'react';
+
+const usePopup = () => {
+  const popupRef = useRef<HTMLElement | null>(null);
+  const buttonRef = useRef<HTMLElement | null>(null);
+  const [isShow, setIsShow] = useState(false);
+
+  useEffect(() => {
+    //팝업 바깥을 클릭하면 isShow 가 false가 됨.
+    //팝업을 여닫는 버튼을 클릭하면 이벤트가 작동하지 않음.
+    const handleMouseClick = (event: MouseEvent) => {
+      if (buttonRef.current?.contains(event.target as Node)) {
+        return;
+      }
+
+      if (popupRef.current?.contains(event.target as Node) === false) {
+        setIsShow(false);
+      }
+    };
+
+    window.addEventListener('click', handleMouseClick);
+
+    return () => {
+      window.removeEventListener('click', handleMouseClick);
+    };
+  }, []);
+
+  return { isShow, setIsShow, popupRef, buttonRef };
+};
+
+export default usePopup;
