3cff9063d50507258073b93db75275412fd112bb
nearworld <artemismars2@outlook.com>
2023-09-20 17:32:50 +0900

    feat: 배포를 위해서 에러 제거

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,5 +1,5 @@
-import Pagination from 'components/commons/Pagination/Pagination';
-import PostCard from 'components/commons/PostCard/PostCard';
+// import Pagination from 'components/commons/Pagination/Pagination';
+// import PostCard from 'components/commons/PostCard/PostCard';
 
 import * as S from './CardList.style';
 
@@ -7,11 +7,11 @@ export const CardList = () => {
   return (
     <>
       <S.Section>
-        {Array.from({ length: 10 }).map((_, idx) => {
-          return <PostCard key={idx} />;
-        })}
+        {/*{Array.from({ length: 10 }).map((_, idx) => {*/}
+        {/*  return <PostCard key={idx} />;*/}
+        {/*})}*/}
       </S.Section>
-      <Pagination />
+      {/*<Pagination />*/}
     </>
   );
 };
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 
+import { CardList } from 'components/CardList/CardList';
 import { CardListMenuBar } from 'components/CardListMenuBar/CardListMenuBar';
 import { Carousel } from 'components/Carousel/Carousel';
 import Profile from 'pages/home/AsideProfile/Profile';
@@ -12,6 +13,7 @@ export const Main = () => {
         <Profile />
       </Sidebar>
       <CardListMenuBar />
+      <CardList />
     </>
   );
 };
