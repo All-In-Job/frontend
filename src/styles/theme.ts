f4905b01a32dd7298a93a2fba443ad4ed7ac67ea
chlgusrb97 <ikolp34@gmail.com>
2023-10-02 23:35:48 +0900

    chore: Emotion 테마 색상 및 폰트 속성 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,29 +1,26 @@
 const palette = {
   orange50: '#fff0eb',
   orange100: '#FFE8DF',
-  orange200: '#febba3',
-  orange300: '#fe9c78',
-  orange400: '#fd895e',
-  orange500: '#fd6b36',
-  orange600: '#e66131',
-  orange700: '#b44c26',
-  orange800: '#8b3b1e',
-  orange900: '#6a2d17',
+  orange200: '#FEC4AF',
+  orange300: '#FEA686',
+  orange400: '#FD895E',
+  orange500: '#FD6B36',
+  orange600: '#CA562B',
+  orange700: '#984020',
 
-  black50: '#e7e7e7',
-  black100: '#b6b5b5',
-  black200: '#929291',
-  black300: '#60605f',
+  black50: '#D0CFCF',
+  black100: '#D0CFCF',
+  black200: '#A0A09F',
+  black300: '#717070',
   black400: '#414140',
   black500: '#121110',
-  black600: '#100f0f',
-  black700: '#0d0c0b',
-  black800: '#0a0909',
-  black900: '#080707',
+  black600: '#0E0E0D',
+  black700: '#0B0A0A',
 
   background: {
     primary: '#EDEDED',
-    secondary: '#E1E2E4',
+    primary50: '#F8F8F8',
+    secondary: '#FFF6F2',
   },
 
   line: {
@@ -32,34 +29,22 @@ const palette = {
   },
 
   secondary: {
-    normal: '#8955F6',
-    strong: '#7A42F0',
-    heavy: '#6C30E8',
-  },
-
-  status: {
-    alert: '#EB5147',
-    caution: '#FA9538',
-    positive: '#3DC061',
+    red: '#FFC1BD',
+    yellow: '#FFE99B',
+    green: '#A7F9B4',
+    blue: '#AAD6FF',
+    purple: '#EEB9FF',
   },
 };
 
 const textStyle = {
-  display01: {
-    fontSize: '36px',
-    lineHeight: '48px',
-  },
-  display02: {
-    fontSize: '32px',
-    lineHeight: '48px',
-  },
-  headLine01: {
-    fontSize: '28px',
-    lineHeight: '38px',
-  },
   headLine02: {
     fontSize: '24px',
     lineHeight: '32px',
+  },
+  title011: {
+    fontSize: '22px',
+    lineHeight: '26px',
   },
   title01: {
     fontSize: '20px',
@@ -75,10 +60,6 @@ const textStyle = {
   },
   body02: {
     fontSize: '15px',
-    lineHeight: '24px',
-  },
-  label01: {
-    fontSize: '16px',
     lineHeight: '24px',
   },
   label02: {
