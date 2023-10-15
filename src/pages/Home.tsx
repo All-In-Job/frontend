cef5812b03c2ba8838b72d80b6b307f8d88ed700
nearworld <artemismars2@outlook.com>
2023-10-15 15:54:05 +0900

    fix: Layout 컴포넌트의 margin-top 값을 동적으로 계산하여 적용되도록 설정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useLayoutEffect, useRef } from 'react';
+
 import { Outlet, useOutlet } from 'react-router-dom';
 
 import { Main } from 'components/Main/Main';
@@ -7,6 +9,22 @@ import * as S from './home.style';
 
 export const Home = () => {
   const outlet = useOutlet();
+
+  const layoutEl = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    const Layout = layoutEl.current;
+    Layout &&
+      setLayoutMarginTop(Layout, getHeaderHeight(Layout.previousSibling as HTMLHeadElement));
+  }, []);
+
+  const getHeaderHeight = (header: HTMLElement | null) => {
+    if (header) return header.offsetHeight;
+    return 0;
+  };
+  const setLayoutMarginTop = (layout: HTMLElement, height: number) => {
+    layout.style.marginTop = height + 'px';
+  };
 
   return (
     <>
