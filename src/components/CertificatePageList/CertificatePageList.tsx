622e11064d0ee5f5ceea546c5244d8c64fcf7de0
codnr <wnd75913@gmail.com>
2023-10-17 13:58:53 +0900

    feat: table 생성 및 마크업

--- src/components/CertificatePageList/CertificatePageList.tsx
+++ src/components/CertificatePageList/CertificatePageList.tsx
@@ -2,19 +2,51 @@ import CertificateItem from 'components/CertificateItem/CertificateItem';
 
 import * as S from './CertificatePageList.styles';
 
+const tableData = {
+  primary: '구분',
+  secondary: `필기원서접수(휴일제외)`,
+  tertiary: '필기시험',
+  quaternary: '필기합격발표일',
+  quinary: '실기원서접수(휴일제외)',
+  senary: '실기시험',
+  septenary: '최종합격발표일',
+};
+
 export const CertificatePageList = () => {
   return (
-    <div>
-      <div>
-        <h3>자격증 정보</h3>
+    <S.Container>
+      <S.CertificateInfo>
+        <S.Title>자격증 정보</S.Title>
         <S.CertificateItemWrapper>
-          <CertificateItem />
+          <CertificateItem location='page' />
         </S.CertificateItemWrapper>
-      </div>
-      <div>
-        <h3>시험정보 정보</h3>
-        <div>Hello, world</div>
-      </div>
-    </div>
+      </S.CertificateInfo>
+
+      <S.Title>시험정보 정보</S.Title>
+      <S.Table>
+        <thead>
+          <S.Tr>
+            <S.Th>{tableData.primary}</S.Th>
+            <S.Th>{tableData.secondary}</S.Th>
+            <S.Th>{tableData.tertiary}</S.Th>
+            <S.Th>{tableData.quaternary}</S.Th>
+            <S.Th>{tableData.quinary}</S.Th>
+            <S.Th>{tableData.senary}</S.Th>
+            <S.Th>{tableData.septenary}</S.Th>
+          </S.Tr>
+        </thead>
+        <tfoot>
+          <S.SecondaryTr>
+            <S.Th>Hello, world</S.Th>
+            <S.Th>Hello, world</S.Th>
+            <S.Th>Hello, world</S.Th>
+            <S.Th>Hello, world</S.Th>
+            <S.Th>Hello, world</S.Th>
+            <S.Th>Hello, world</S.Th>
+            <S.Th>Hello, world</S.Th>
+          </S.SecondaryTr>
+        </tfoot>
+      </S.Table>
+    </S.Container>
   );
 };
