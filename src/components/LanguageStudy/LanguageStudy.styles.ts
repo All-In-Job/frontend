bf5b48765a95c9821ae319626b21de50d99e52db
hyundee <hyundee9@gmail.com>
2023-10-17 20:47:22 +0900

    fix: 날짜에 따른 tag 기능 구현 및 json 파일 수정

--- src/components/LanguageStudy/LanguageStudy.styles.ts
+++ src/components/LanguageStudy/LanguageStudy.styles.ts
@@ -6,7 +6,7 @@ export const LanguageStudyContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 32px 21px;
-  color: #414140;
+  color: var(--black-400, #414140);
   font-family: SUIT;
   font-size: 20px;
   font-style: normal;
@@ -50,7 +50,7 @@ export const Tag = styled.div`
     height: 32px;
     padding: 4px 8px;
     border-radius: 4px;
-    background: #fff;
+    background: var(--black-white-wh, #fff);
     font-size: 15px;
     line-height: 24px;
   }
@@ -72,16 +72,17 @@ export const Imminent = styled.span`
   border: 1px solid var(--orange-500, #fd6b36);
 `;
 
-export const BookmarkIcon = styled(Bookmark)`
+export const BookmarkIcon = styled(Bookmark)<{ 'data-ispick': boolean }>`
   cursor: pointer;
+  path {
+    fill: ${props => (props['data-ispick'] ? props.theme.palette.black200 : '#fff')};
+    stroke: ${props =>
+      props['data-ispick'] ? props.theme.palette.black100 : props.theme.palette.black200};
+  }
 `;
 
-export const ExamDate = styled.div``;
-export const Deadline = styled.div``;
-export const Schedule = styled.div``;
-
 export const Title = styled.div`
-  color: #121110;
+  color: var(--black-500, #121110);
   font-feature-settings: 'ss10' on;
   font-size: 24px;
   line-height: 32px;
@@ -92,13 +93,17 @@ export const ActivateBtn = styled.button`
   width: 100%;
   display: flex;
   height: 40px;
-  padding: 8px 16px;
   justify-content: center;
   align-items: center;
   border-radius: 4px;
-  color: #f6f6f6;
-  background-color: #fd6b36;
-  cursor: pointer;
+  color: var(--title-white, #f6f6f6);
+  background: var(--orange-500, #fd6b36);
+  a {
+    width: 100%;
+    height: 100%;
+    padding: 8px 16px;
+    cursor: pointer;
+  }
 `;
 
 export const DisabledBtn = styled.button`
@@ -109,7 +114,11 @@ export const DisabledBtn = styled.button`
   justify-content: center;
   align-items: center;
   border-radius: 4px;
-  color: #f6f6f6;
-  background-color: #d0cfcf;
+  color: var(--background-primary-50, #f8f8f8);
+  background: var(--black-100, #d0cfcf);
   pointer-events: none;
 `;
+
+export const ExamDate = styled.div``;
+export const Deadline = styled.div``;
+export const Schedule = styled.div``;
--- src/components/LanguageStudy/LanguageStudy.tsx
+++ src/components/LanguageStudy/LanguageStudy.tsx
@@ -1,41 +1,82 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
 import * as S from './LanguageStudy.styles';
 
-const LanguageStudy = () => {
-  const now = new Date();
-  const open = new Date('2023.10.20 12:00');
-  const close = new Date('2023.10.31 10:00');
+type TGetData = {
+  data: LanguageProps[];
+};
 
-  console.log(close, open);
+type LanguageProps = {
+  id: string;
+  path: string;
+  test: string;
+  homPage: string;
+  examDate: string;
+  openDate: string;
+  closeDate: string;
+  Dday: string;
+};
+
+const LanguageStudy = (data: TGetData) => {
+  const [bookmark, setBookmark] = useState(false);
+
+  const onBookmark = () => {
+    setBookmark(prev => !prev);
+  };
+
+  const now = new Date();
+
+  const timestamp = (date: string) => {
+    const weekday = ['일', '월', '화', '수', '목', '금', '토'];
+    const currentDate = new Date(date);
+
+    const year = currentDate.getFullYear();
+    const month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+    const day = ('0' + currentDate.getDate()).slice(-2);
+    const hour = ('0' + currentDate.getHours()).slice(-2);
+    const minute = ('0' + currentDate.getMinutes()).slice(-2);
+    const week = weekday[currentDate.getDay()];
+
+    return `${year}.${month}.${day} (${week}) ${hour}:${minute}`;
+  };
+
   return (
     <S.LanguageStudyContainer>
-      <S.LanguageStudyWrapper>
-        <S.TextBox>
-          <S.TagWrapper>
-            <S.Tag>
-              <S.Disabled>접수예정</S.Disabled>
-              <S.Activate>접수중</S.Activate>
-              <S.Imminent>마감임박</S.Imminent>
-            </S.Tag>
-            <Link to={'/login'}>
-              <S.BookmarkIcon />
-            </Link>
-          </S.TagWrapper>
-          <S.Title>TOEIC (Reading, Listening)</S.Title>
-          <S.Schedule>
-            <S.ExamDate>{`시험일시 : 2023.08.25 (일) 12:00`}</S.ExamDate>
-            <S.Deadline>{`접수마감 : 2023.06.25 (월) 10:00`}</S.Deadline>
-          </S.Schedule>
-        </S.TextBox>
-        {open > now ? (
-          <S.DisabledBtn>{`2023.10.20 접수예정`}</S.DisabledBtn>
-        ) : (
-          <S.ActivateBtn>접수하기</S.ActivateBtn>
-        )}
-      </S.LanguageStudyWrapper>
+      {data.data.map(el => {
+        return (
+          <S.LanguageStudyWrapper key={el.id}>
+            <S.TextBox>
+              <S.TagWrapper>
+                <S.Tag>
+                  {new Date(el.openDate) > now ? (
+                    <S.Disabled>접수예정</S.Disabled>
+                  ) : (
+                    <S.Activate>접수중</S.Activate>
+                  )}
+                  {el.Dday === 'D-3' ? <S.Imminent>마감임박</S.Imminent> : null}
+                </S.Tag>
+                <Link to={'/login'} onClick={onBookmark}>
+                  <S.BookmarkIcon data-ispick={bookmark} />
+                </Link>
+              </S.TagWrapper>
+              <S.Title>{el.test}</S.Title>
+              <S.Schedule>
+                <S.ExamDate>{`시험일시 : ${timestamp(el.examDate)}`}</S.ExamDate>
+                <S.Deadline>{`접수마감 : ${timestamp(el.closeDate)}`}</S.Deadline>
+              </S.Schedule>
+            </S.TextBox>
+            {new Date(el.openDate) > now ? (
+              <S.DisabledBtn>{`${timestamp(el.openDate).slice(0, 10)} 접수예정`}</S.DisabledBtn>
+            ) : (
+              <S.ActivateBtn>
+                <Link to={`${el.homPage}`}>접수하기</Link>
+              </S.ActivateBtn>
+            )}
+          </S.LanguageStudyWrapper>
+        );
+      })}
     </S.LanguageStudyContainer>
   );
 };
