da82a3c9058a517427f5123be0c027b80740070c
chlgusrb97 <ikolp34@gmail.com>
2023-10-19 07:22:43 +0900

    feat: 카테고리 클릭 처리 및 선택된 카테고리에 해시 태그 설정 코드 공용화

--- src/pages/menu/Menu.tsx
+++ src/pages/menu/Menu.tsx
@@ -1,18 +1,41 @@
+import { useState } from 'react';
+
 import styled from '@emotion/styled';
 import { Outlet, useParams } from 'react-router-dom';
 
-import { MenuId, getMenuById } from './menuList';
+import MultiSelectTags from 'components/Badge';
+import { HashTagData } from 'components/Badge/type';
+
+import { MenuId, MenuItems, getMenuById } from './menuList';
 
 const Menu = () => {
-  const { menuName } = useParams();
+  const [selectedCategory, setSelectedCategory] = useState<HashTagData[] | null>(null);
 
+  const { menuName } = useParams();
   const foundMenuList = getMenuById(menuName! as MenuId);
 
-  console.log(foundMenuList);
+  const handleCategoryClick = (category: MenuItems) => {
+    if (!category || !category.keywords) return;
+
+    const hashTags = category.keywords.map(keyword => ({ id: keyword, title: keyword }));
+
+    setSelectedCategory(hashTags);
+  };
 
   return (
     <MenuWrapper>
-      <MenuHeadContent></MenuHeadContent>
+      <CategoryList>
+        {foundMenuList?.items.map(item => (
+          <li key={item.category} onClick={() => handleCategoryClick(item)}>
+            {item.category}
+          </li>
+        ))}
+      </CategoryList>
+      <MenuHeadContent>
+        {selectedCategory && (
+          <MultiSelectTags title='키워드' hashTagList={selectedCategory} key={foundMenuList?.id} />
+        )}
+      </MenuHeadContent>
 
       <Outlet />
     </MenuWrapper>
@@ -23,7 +46,19 @@ export default Menu;
 
 const MenuWrapper = styled.div`
   grid-column: span 12;
+  margin-top: 32px;
 `;
 const MenuHeadContent = styled.div`
   padding-bottom: 32px;
+`;
+
+const CategoryList = styled.ul`
+  display: flex;
+
+  > li {
+    cursor: pointer;
+    border: 1px solid #000;
+    padding: 3px;
+    margin-right: 3px;
+  }
 `;
