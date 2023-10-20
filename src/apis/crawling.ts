9235323dfd71dd72627fa2d2675f8d55881b37cc
nearworld <artemismars2@outlook.com>
2023-10-20 15:00:37 +0900

    refactor: CrawlingQuery 타입 주석처리 되어있던 것 제거

--- src/apis/crawling.ts
+++ src/apis/crawling.ts
@@ -1,12 +1,5 @@
 import { crawlingApi } from './index';
 
-// type CrawlingQuery = {
-//   path?: string;
-//   count?: number;
-//   page?: string;
-//   location?: string;
-// }
-
 export const requestCrawlingData: (menu: string, queries: string) => void = async (menu, queries) =>
   await crawlingApi({
     method: 'get',
