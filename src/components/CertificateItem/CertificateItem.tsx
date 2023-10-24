77cad944c46876ec0eb37a0b2f4ec606730f004c
codnr <wnd75913@gmail.com>
2023-10-24 12:35:10 +0900

    refactor: CertifiateItem type 수정

--- src/components/CertificateItem/CertificateItem.tsx
+++ src/components/CertificateItem/CertificateItem.tsx
@@ -1,49 +1,10 @@
+import { Certificate } from 'types/certificate.type';
+
 import * as S from './CertificateItem.styles';
 
-type ExamSchedule = {
-  id: string;
-  turn: string;
-  wtReceipt: string;
-  wtDday: string;
-  wtResultDay: string;
-  ptReceipt: string;
-  ptDday: string;
-  resultDay: string;
-};
-
-type Certificate = {
-  title: string;
-  engJmNm?: string | null;
-  relatedDepartment: string;
-  scrap: number;
-  view: number;
-  location: 'main' | 'page';
-  institution: string;
-  examSchedules: ExamSchedule[];
-};
-
-// {
-//   "data": [
-//     {
-//       "id": "자격증 id : adfhttwerqwrasxczx",
-//       "title": "자격증명 : 에너지산업기사",
-//       "view": 100,
-//       "scrap": 0,
-//       "relatedDepartment": "과련부처 : 통겨청",
-//       "institution": "시행기관 : 한국산업인력공단",
-//       "image": "...",
-//       "examSchedules": {
-//         '구분':"a",
-//         "필기원서접수(휴일제외)":"a",
-//         '필기시험':"a",
-//         '필기합격발표일':"a",
-//         '실기원서접수(휴일제외)':"a",
-//         '실기시험':"a",
-//         '최종합격발표일':"a",
-//       }
-//     }
-//   ]
-// }
+interface CertificateItemProps extends Certificate {
+  location: 'page' | 'main';
+}
 
 function CertificateItem({
   location,
@@ -52,7 +13,7 @@ function CertificateItem({
   relatedDepartment,
   scrap,
   view = 0,
-}: Certificate) {
+}: CertificateItemProps) {
   return (
     <S.CertificateListContainer location={location}>
       <S.CertificateInfo>
