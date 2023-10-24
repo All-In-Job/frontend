a3bf6e75af525c8b7861078fb54abf4713cdc5f7
hyundee <hyundee9@gmail.com>
2023-10-24 20:01:20 +0900

    refactor : 파일 분리 및 코드 수정

---
+++ src/components/InternPageList/InternPageList.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+
+import { useParams } from 'react-router-dom';
+import { Inter } from 'types/intern.type';
+
+import { requestCrawlingData } from 'apis/crawling';
+
+import { InternPageItem } from './InternInfo/InternPageItem';
+import * as S from './InternPageList.styles';
+
+export const InternPageList = () => {
+  const Table = ['기업명', '공고명', '지역', '마감일', '조회수', '스크랩'];
+
+  const { menuName } = useParams();
+
+  const [InternPageList, setInternPageList] = useState<Inter[]>([]);
+
+  const queries = {
+    path: menuName,
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await requestCrawlingData(menuName as string, queries);
+        setInternPageList(res.data.data);
+        console.log(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [menuName]);
+
+  return (
+    <S.InternContainer>
+      <S.TableTitle>
+        {Table.map(el => {
+          return <S.Heading>{el}</S.Heading>;
+        })}
+      </S.TableTitle>
+      {InternPageList.map(el => (
+        <InternPageItem
+          key={el.id}
+          id={el.id}
+          institution={el.institution}
+          view={el.view}
+          mainImage={el.mainImage}
+          scrap={el.scrap}
+          Dday={el.Dday}
+          location={el.location}
+          title={el.title}
+        />
+      ))}
+    </S.InternContainer>
+  );
+};
