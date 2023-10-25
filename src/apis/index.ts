6daeb1a0ccb834b29ce6da12f6685b5136f39d7f
nearworld <artemismars2@outlook.com>
2023-10-25 14:12:16 +0900

    fix: Mock 데이터용 api 함수는 그냥 axios 함수 바로 호출하는 식으로 변경

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,33 +1,19 @@
 import axios from 'axios';
 
 // production mode
-const prodUrl = {
+const baseURLs = {
   competition: import.meta.env.VITE_API_COMPETITION_BASE_URL,
   intern: import.meta.env.VITE_API_COMPETITION_BASE_URL,
   communityApi: import.meta.env.VITE_API_COMMUNITY_BASE_URL,
   crawlingApi: import.meta.env.VITE_API_CRAWLING, // /crawling
 };
-// development mode
-const devUrl = {
-  competition: '/mocks/compeition.json',
-  intern: '/mocks/intern.json',
-  communityApi: '/mocks/community.json',
-  crawlingApi: '/mocks/findeCrawling.json',
-  mainCrawlingApi: '/mocks/crawling.json',
-};
-
-// production mode 인지 development mode 인지 체크하여 어떤 url 객체를 사용할지 결정
-// export const baseURL = import.meta.env.DEV ? devUrl : prodUrl;
-export const baseURL = import.meta.env.DEV ? prodUrl : devUrl;
-
 
 function createAxiosInstance(url: string) {
   return axios.create({ baseURL: url });
 }
 
-export const competitionApi = createAxiosInstance(baseURL.competition);
-export const internApi = createAxiosInstance(baseURL.intern);
+export const competitionApi = createAxiosInstance(baseURLs.competition);
+export const internApi = createAxiosInstance(baseURLs.intern);
 
-export const communityApi = createAxiosInstance(baseURL.communityApi);
-export const crawlingApi = createAxiosInstance(baseURL.crawlingApi);
-
+export const communityApi = createAxiosInstance(baseURLs.communityApi);
+export const crawlingApi = createAxiosInstance(baseURLs.crawlingApi);
