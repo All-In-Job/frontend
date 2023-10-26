e00c1874292874c19b9292299421f31a5691c80c
codnr <wnd75913@gmail.com>
2023-10-26 15:13:54 +0900

    feat: dDay 표시하는 기능 구현

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -6,26 +6,39 @@ import { Certificate } from 'types/certificate.type';
 
 import { requestDetailCrawlingData } from 'apis/detailCrawling';
 
+// import * as S from './index.styles';
+
 export const DetailPage = () => {
   const { menuName, detailId } = useParams();
   const [detailData, setDetailData] = useState<Certificate>();
+  const [dDay, setDDay] = useState('');
 
+  const DdayCalculator = (date: string) => {
+    const stringDate = date.toString();
+    const currentDate = new Date().getTime();
+    const Dday = new Date(stringDate.split('~')[0]).getTime();
+    const result = Math.ceil((Dday - currentDate) / (1000 * 60 * 60 * 24));
+    return result.toString();
+  };
+  console.log(detailData);
   useEffect(() => {
     (async () => {
       try {
         const res = await requestDetailCrawlingData(menuName, detailId);
         setDetailData(res.data.data);
+        if (res.data.data.examSchedules) {
+          setDDay(DdayCalculator(res.data.data.examSchedules[0].wtReceipt));
+        }
       } catch (error) {
         console.error(error);
       }
     })();
   }, [menuName, detailId]);
 
-  console.log(detailData);
   return (
     <div>
       <div>
-        <div></div>
+        <div>{`D-${dDay}`}</div>
         <h1></h1>
         <button>
           <Bookmark />
