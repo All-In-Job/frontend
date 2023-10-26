a228550b1584a7b7a259fc435dbae5de8a0b8a85
codnr <wnd75913@gmail.com>
2023-10-26 16:34:55 +0900

    feat: icon 추가

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,34 +1,34 @@
 import { useEffect, useState } from 'react';
 
-import { ReactComponent as Bookmark } from 'assets/icons/solid_bookmark.svg';
 import { useParams } from 'react-router-dom';
 import { Certificate } from 'types/certificate.type';
 
 import { requestDetailCrawlingData } from 'apis/detailCrawling';
+
+import { ReactComponent as CountBookmark } from './res/icon-bookmark.svg';
+import { ReactComponent as HorizontalRuleIcon } from './res/icon-horizontal_rule.svg';
+import { ReactComponent as SolidBookmarkIcon } from './res/icon-solid-bookmark.svg';
+import { ReactComponent as ViewIcon } from './res/icon-view.svg';
 
 // import * as S from './index.styles';
 
 export const DetailPage = () => {
   const { menuName, detailId } = useParams();
   const [detailData, setDetailData] = useState<Certificate>();
-  const [dDay, setDDay] = useState('');
 
-  const DdayCalculator = (date: string) => {
-    const stringDate = date.toString();
-    const currentDate = new Date().getTime();
-    const Dday = new Date(stringDate.split('~')[0]).getTime();
-    const result = Math.ceil((Dday - currentDate) / (1000 * 60 * 60 * 24));
-    return result.toString();
-  };
-  console.log(detailData);
+  // const DdayCalculator = (date: string) => {
+  //   const stringDate = date.toString();
+  //   const currentDate = new Date().getTime();
+  //   // const Dday = new Date(stringDate.split('~')[0]).getTime();
+  //   const result = Math.ceil((Dday - currentDate) / (1000 * 60 * 60 * 24));
+  //   return result.toString();
+  // };
+
   useEffect(() => {
     (async () => {
       try {
         const res = await requestDetailCrawlingData(menuName, detailId);
         setDetailData(res.data.data);
-        if (res.data.data.examSchedules) {
-          setDDay(DdayCalculator(res.data.data.examSchedules[0].wtReceipt));
-        }
       } catch (error) {
         console.error(error);
       }
@@ -38,12 +38,23 @@ export const DetailPage = () => {
   return (
     <div>
       <div>
-        <div>{`D-${dDay}`}</div>
-        <h1></h1>
+        <div>{`D-`}</div>
+        <h1>{detailData?.title}</h1>
         <button>
-          <Bookmark />
+          <SolidBookmarkIcon />
           <h4>스크랩</h4>
         </button>
+        <div>
+          <div>
+            <CountBookmark />
+            <p></p>
+          </div>
+          <HorizontalRuleIcon />
+          <div>
+            <ViewIcon />
+            <p></p>
+          </div>
+        </div>
       </div>
     </div>
   );
