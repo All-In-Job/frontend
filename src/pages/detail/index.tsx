b1792690b114e85783f247642acdbed8df8bebd3
codnr <wnd75913@gmail.com>
2023-10-26 13:14:20 +0900

    feat: detail page 생성

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,3 +1,19 @@
+import { ReactComponent as Bookmark } from 'assets/icons/solid_bookmark.svg';
+import { useParams } from 'react-router-dom';
+
 export const DetailPage = () => {
-  return <div>hello</div>;
+  const { detailId } = useParams();
+  console.log(detailId);
+  return (
+    <div>
+      <div>
+        <div></div>
+        <h1></h1>
+        <button>
+          <Bookmark />
+          <h4>스크랩</h4>
+        </button>
+      </div>
+    </div>
+  );
 };
