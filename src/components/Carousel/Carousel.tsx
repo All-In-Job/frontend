b612b8eb910701b456cff0fb3899914222aa8ecc
nearworld <artemismars2@outlook.com>
2023-10-28 20:41:27 +0900

    feat: 이미지 슬라이드 배열에 예시 이미지 링크 등록

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import styled from '@emotion/styled';
 
 import theme from 'styles/theme';
@@ -5,13 +7,20 @@ import theme from 'styles/theme';
 // import { CarouselButton } from './CarouselButton';
 import { CarouselItemSelector } from './CarouselItemSelector';
 
+const images = [
+  'https://images.pexels.com/photos/933054/pexels-photo-933054.jpeg?cs=srgb&dl=pexels-joyston-judah-933054.jpg&fm=jpg',
+  'https://wallpapers.com/images/hd/high-resolution-nature-mountains-landscape-5n3jp9psonuimymd.jpg',
+  'https://wallpapers.com/images/hd/mountain-top-t6qhv1lk4j0au09t.jpg',
+];
+
 export const Carousel = () => {
-  const images = [1, 2, 3];
+  const [currentImage] = useState(images[0]);
 
   return (
     <StyledContainer>
       {/*<CarouselButton direction='right' />*/}
       {/*<CarouselButton direction='left' />*/}
+      <StyledImage currentImage={currentImage} />
       <CarouselItemSelector length={images.length} />
     </StyledContainer>
   );
@@ -22,4 +31,11 @@ const StyledContainer = styled.div`
   background-color: ${theme.palette.orange100};
   position: relative;
   height: 439px;
+`;
+const StyledImage = styled.div<{ currentImage: string }>`
+  width: 100%;
+  height: 100%;
+  background-image: ${props => (props.currentImage ? `url(${props.currentImage})` : null)};
+  background-size: cover;
+  background-position: center;
 `;
