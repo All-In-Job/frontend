ae481846a52dfaf98c766ea83adb837f767c05d3
codnr <wnd75913@gmail.com>
2023-10-31 15:26:20 +0900

    design: detail style 수정

--- src/pages/detail/index.styles.ts
+++ src/pages/detail/index.styles.ts
@@ -1,3 +1,6 @@
 import styled from '@emotion/styled';
 
+export const Container = styled.div`
+  grid-column: span 12;
+`;
 export const Title = styled.div``;
--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
+import { ExamSchedule } from 'types/certificate.type';
 
 import { ResponseData, requestDetailCrawlingData } from 'apis/detailCrawling';
 import { CertificateDetailInfo } from 'components/CertificateDetailInfo/CertificateDetailInfo';
+import { CertificateExamSchedule } from 'components/CertificateExamSchedule/CertificateExamSchedule';
 import { DetailPageInfo } from 'components/DetailPageInfo';
 
 import * as S from './index.styles';
@@ -11,6 +13,10 @@ import * as S from './index.styles';
 export const DetailPage = () => {
   const { menuName, detailId } = useParams();
   const [detailData, setDetailData] = useState<ResponseData>();
+
+  const pathProps = (data: ExamSchedule[]) => {
+    return data && data[0];
+  };
 
   useEffect(() => {
     (async () => {
@@ -42,9 +48,20 @@ export const DetailPage = () => {
             institution={detailData.institution}
           />
         </DetailPageInfo>
-        <div>
+        <S.Container>
           <S.Title>{'시험일정'}</S.Title>
-        </div>
+          <CertificateExamSchedule
+            id={pathProps(detailData.examSchedules)?.id}
+            key={pathProps(detailData.examSchedules)?.id}
+            turn={pathProps(detailData.examSchedules)?.turn}
+            wtReceipt={pathProps(detailData.examSchedules)?.wtReceipt}
+            wtDday={pathProps(detailData.examSchedules)?.wtDday}
+            wtResultDay={pathProps(detailData.examSchedules)?.wtResultDay}
+            ptReceipt={pathProps(detailData.examSchedules)?.ptReceipt}
+            ptDday={pathProps(detailData.examSchedules)?.ptDday}
+            resultDay={pathProps(detailData.examSchedules)?.resultDay}
+          />
+        </S.Container>
       </>
     );
 };
