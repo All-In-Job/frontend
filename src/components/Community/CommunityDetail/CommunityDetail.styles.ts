b328d4a354aa994f35ffc88843233016c66b0a6e
codnr <wnd75913@gmail.com>
2023-11-02 15:40:35 +0900

    feat: CommunityDetail 페이지 마크업

---
+++ src/components/Community/CommunityDetail/CommunityDetail.styles.ts
@@ -0,0 +1,110 @@
+import styled from '@emotion/styled';
+import { ReactComponent as Horizontal } from 'assets/icons/icon-horizontal_rule.svg';
+
+import theme from 'styles/theme';
+
+import { ReactComponent as Like } from './res/icon-like-solid.svg';
+import { ReactComponent as Share } from './res/icon-share.svg';
+
+const { palette } = theme;
+const { textStyle } = theme;
+
+export const Container = styled.div`
+  display: grid;
+  grid-column: span 12;
+  gap: 32px;
+  padding: 32px 0;
+  font-family: Bold;
+`;
+
+export const Head = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  background-color: ${palette.background.secondary};
+  padding: 32px;
+  border-radius: 12px;
+  ${textStyle.headLine02}
+  color: ${palette.black500};
+`;
+
+export const Category = styled.h2`
+  background-color: white;
+  padding: 8px 16px;
+  border: solid 2px ${palette.orange500};
+  border-radius: 4px;
+  ${textStyle.body01}
+  color: ${palette.orange500};
+`;
+
+export const Body = styled.article`
+  border: 2px solid ${palette.orange300};
+  border-radius: 12px;
+  padding: 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
+
+export const ArticleHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const Profile = styled.img`
+  background-color: ${palette.background.primary};
+  border-radius: 100%;
+  height: 44px;
+  width: 44px;
+`;
+
+export const Nickname = styled.div`
+  ${textStyle.title02}
+`;
+
+export const TimeDiff = styled.div`
+  ${textStyle.label03}
+  color: ${palette.black200};
+`;
+
+export const ArticleTitle = styled.div`
+  ${textStyle.title11}
+  color: ${palette.black500};
+`;
+export const Article = styled.div`
+  ${textStyle.title02}
+  font-family: Medium;
+  color: ${palette.black500};
+  height: 374px;
+`;
+
+export const ArticleFooter = styled.div`
+  border-top: 1px solid ${palette.black100};
+  display: flex;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const IconBtn = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  margin-top: 24px;
+  ${textStyle.title01}
+  color: ${palette.black200};
+`;
+
+export const CountContainer = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const HorizontalIcon = styled(Horizontal)``;
+export const HorizontalIcon = styled(Horizontal)``;
+export const HorizontalIcon = styled(Horizontal)``;
+export const LikeSolidIcon = styled(Like)``;
+export const ShareSolidIcon = styled(Share)``;
--- src/components/Community/CommunityDetail/CommunityDetail.tsx
+++ src/components/Community/CommunityDetail/CommunityDetail.tsx
@@ -1,9 +1,37 @@
+import * as S from './CommunityDetail.styles';
+
 export const CommunityDetail = () => {
   return (
-    <div>
-      <div>
-        <h1></h1> <h2>{'category'}</h2>
-      </div>
-    </div>
+    <S.Container>
+      <S.Head>
+        <h1>취준job담</h1> <S.Category>{'category'}</S.Category>
+      </S.Head>
+      <S.Body>
+        <S.ArticleHeader>
+          <S.Profile />
+          <div>
+            <S.Nickname>hello</S.Nickname>
+            <S.TimeDiff>12잔</S.TimeDiff>
+          </div>
+        </S.ArticleHeader>
+
+        <S.ArticleTitle>is Title</S.ArticleTitle>
+        <S.Article>contents</S.Article>
+
+        <S.ArticleFooter>
+          <S.ButtonContainer>
+            <S.IconBtn>
+              <S.LikeSolidIcon /> <p>좋아요</p>
+            </S.IconBtn>
+            <S.IconBtn>
+              <S.ShareSolidIcon /> <p>공유하기</p>
+            </S.IconBtn>
+          </S.ButtonContainer>
+          <S.CountContainer>
+            <S.HorizontalIcon />
+          </S.CountContainer>
+        </S.ArticleFooter>
+      </S.Body>
+    </S.Container>
   );
 };
