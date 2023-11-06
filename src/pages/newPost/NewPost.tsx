1009fdd4f4ece628c0a3e753d0b93a9c9e595502
codnr <wnd75913@gmail.com>
2023-11-06 11:38:57 +0900

    design: option 클릭시 색상 변경

--- src/pages/newPost/NewPost.tsx
+++ src/pages/newPost/NewPost.tsx
@@ -1,72 +1,14 @@
-import { ReactComponent as ExpendMoreIcon } from 'assets/icons/icon_expend_more.svg';
-import { ReactComponent as ReversExpendMoreIcon } from 'assets/icons/icon_revers_expend_more.svg';
-import Select, { StylesConfig, components, DropdownIndicatorProps } from 'react-select';
-
-import theme from 'styles/theme';
+import { DropDownSelect } from 'components/commons/Select/Select';
 
 import * as S from './newPost.styles';
-const { textStyle } = theme;
-const { palette } = theme;
 
 const options = [
   { value: '자격증/어학', label: '공모전/대외활동' },
-  { value: '인턴', label: '공모전/대외활동' },
-  { value: '스터디', label: '공모전/대외활동' },
+  { value: '인턴', label: '인턴' },
+  { value: '스터디', label: '스터디' },
   { value: '취업선배 Q&A', label: '취업선배 Q&A' },
   { value: '자유게시판', label: '자유게시판' },
 ];
-
-const DropdownIndicator = (props: DropdownIndicatorProps) => {
-  return (
-    <components.DropdownIndicator {...props}>
-      <div
-        style={{
-          width: '24px',
-          height: '24px',
-        }}
-      >
-        {props.selectProps.menuIsOpen ? <ReversExpendMoreIcon /> : <ExpendMoreIcon />}
-      </div>
-    </components.DropdownIndicator>
-  );
-};
-
-const customStyles: StylesConfig = {
-  container: provided => ({
-    ...provided,
-  }),
-  control: (provided, state) => ({
-    ...provided,
-    '&:hover': 'none',
-    border:
-      state.menuIsOpen || state.isFocused
-        ? `${palette.orange500} 2px solid`
-        : `${palette.black200} 1px solid`,
-    borderRadius: state.menuIsOpen ? '4px 4px 0px 0px' : provided.borderRadius,
-    boxShadow: state.menuIsOpen ? '0px 3px 6px #00000047' : 'none',
-  }),
-  menu: provided => ({
-    ...provided,
-    backgroundColor: 'white',
-    marginTop: '1px',
-    borderRadius: '0px',
-  }),
-  menuList: provided => ({
-    ...provided,
-    padding: '0px',
-  }),
-  option: (provided, { isSelected }) => ({
-    ...provided,
-    ...textStyle.title02,
-    fontFamily: 'Medium',
-    color: palette.black500,
-    backgroundColor: isSelected ? palette.background.primary : provided.backgroundColor,
-  }),
-  indicatorSeparator: provided => ({
-    ...provided,
-    display: 'none',
-  }),
-};
 
 export const NewPost = () => {
   return (
@@ -75,12 +17,7 @@ export const NewPost = () => {
       <form>
         <S.SelectContainer>
           <label htmlFor='select'>주제</label>
-          <Select
-            openMenuOnFocus={false}
-            options={options}
-            components={{ DropdownIndicator }}
-            styles={customStyles}
-          />
+          <DropDownSelect options={options} />
         </S.SelectContainer>
         <div>
           <label></label>
