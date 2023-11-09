7d1da0a10bfc619614e34cbaf150c83d38a95dfe
nearworld <artemismars2@outlook.com>
2023-11-09 21:06:05 +0900

    feat: 내정보 수정 모달 내용을 하위 컴포넌트로 구성

--- src/components/Modals/MyInfoUpdateModal/MyInfoUpdateModal.tsx
+++ src/components/Modals/MyInfoUpdateModal/MyInfoUpdateModal.tsx
@@ -1,14 +1,27 @@
-// import styled from '@emotion/styled';
+import styled from '@emotion/styled';
 
 import { ModalBackground } from 'components/Modals/ModalBackground';
 
-// const MODAL_WIDTH_RATIO = 2.67;
-// const MODAL_HEIGHT_RATIO = 1.35;
+import { InterestFieldSection } from './InterestFieldSection';
+import { NicknameSection } from './NicknameSection';
+import { ProfileImageSection } from './ProfileImageSection';
+
+const MODAL_WIDTH_RATIO = 2.67;
+const MODAL_HEIGHT_RATIO = 1.35;
 
 export const MyInfoUpdateModal = () => {
   return (
     <ModalBackground>
-      <div>MyInfoUpdateModal</div>
+      <StyledContainer>MyInfoUpdateModal</StyledContainer>
+      <ProfileImageSection />
+      <NicknameSection />
+      <InterestFieldSection />
     </ModalBackground>
   );
 };
+
+const StyledContainer = styled.div`
+  background-color: white;
+  width: ${window.innerWidth / MODAL_WIDTH_RATIO}px;
+  height: ${window.innerHeight / MODAL_HEIGHT_RATIO}px;
+`;
