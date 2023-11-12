7575011e29f4697a5c10e7344c64b64dfb42b1fc
nearworld <artemismars2@outlook.com>
2023-11-12 20:55:36 +0900

    feat: formState 상태에 interestTags 속성 추가하여 선택된 관심분야 데이터 상태로 관리

--- src/hooks/useInterestForm.tsx
+++ src/hooks/useInterestForm.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
 
+type InterestTagsType = Array<{
+  name: (typeof tags)[number];
+  keyWords: string[];
+}>;
+
 type InterestFormState = {
   subMajor: string;
+  interestTags: InterestTagsType;
 };
+
+const tags = ['공모전', '대외활동', '자격증', '어학', '인턴'] as const;
 
 export const useInterestForm = () => {
   const [formState, setFormState] = useState<InterestFormState>({
     subMajor: '',
+    interestTags: [...tags.map(tag => ({ name: tag, keyWords: [] }))],
   });
 
   console.log(formState);
