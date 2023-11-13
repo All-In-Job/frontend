f158688db2c3640dc80a5b37d5fa1ec0f653a5f8
nearworld <artemismars2@outlook.com>
2023-11-13 22:56:55 +0900

    fix: 로그인 화면 높이 브라우저 창에 맞추도록 재조정

--- src/components/Layout/layout.styles.ts
+++ src/components/Layout/layout.styles.ts
@@ -1,15 +1,15 @@
 import styled from '@emotion/styled';
 
-export const COLUMN_WIDTH = 78;
+export const COLUMN_WIDTH = 4;
 
 export const Layout = styled.div`
   //background-color: azure;
   //min-height: 100vh;
   display: grid;
-  grid-template-columns: repeat(12, ${COLUMN_WIDTH}px);
+  grid-template-columns: repeat(12, ${COLUMN_WIDTH}vw);
   column-gap: 24px;
   justify-content: center;
-  margin-bottom: 12px;
+  //margin-bottom: 12px;
   height: 100%;
 `;
 
