b21a54f16c9a238b51e68f7a4bd8830731c8945d
jun1485 <wnwjdwns1@naver.com>
2023-11-17 01:46:30 +0900

    feat:내정보 페이지 로그인 정보 퍼블리싱

--- src/pages/myInfo/myInfo.style.ts
+++ src/pages/myInfo/myInfo.style.ts
@@ -17,10 +17,100 @@ export const LoginInfoWrapper = styled.div`
 `;
 
 export const ProfileBox = styled.div`
-  margin: auto;
+  width: 180px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  background-color: #f8f8f8;
+  border-radius: 8px;
+  padding: 8px 0;
 `;
 
 export const ProfileImgWrapper = styled.div`
+  width: 64px;
+  height: 64px;
   border-radius: 100%;
+  border: 3px solid #fd6b36;
+  margin-bottom: 8px;
+`;
+
+export const ProfileNickname = styled.p`
+  font-size: 16px;
+  font-weight: 700;
+  margin-bottom: 12px;
+  padding: 5px 8px;
+`;
+
+export const ProfileEditBtn = styled.button`
+  font-size: 11px;
+  font-weight: 700;
+  background-color: #ededed;
+  color: #a0a09f;
+  border-radius: 4px;
+  padding: 5px 8px;
+`;
+
+export const InfoBox = styled.div`
+  width: 282px;
+  background-color: #f8f8f8;
+  border-radius: 8px;
+  padding: 16px 24px;
+`;
+
+export const basicInfoTitle = styled.p`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px;
+  margin-bottom: 16px;
+`;
+
+export const basicInfoWrapper = styled.div`
+  display: flex;
+  gap: 32px;
+`;
+
+export const Id = styled.p`
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 20px;
+`;
+
+export const UserId = styled.p`
+  color: #717070;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 20px;
+`;
+
+export const InterestFieldBox = styled.div`
+  flex-grow: 1;
+  width: 282px;
+  background-color: #f8f8f8;
+  border-radius: 8px;
+  padding: 16px 24px;
+`;
+
+export const InterestFieldTitle = styled.p`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px;
+  margin-bottom: 16px;
+`;
+
+export const InterestTagWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
+
+export const InterestTag = styled.div`
   border: 1px solid #fd6b36;
+  border-radius: 4px;
+  padding: 8px;
 `;
