576e29d489d7708a77266aed0926218af55164a5
chlgusrb97 <ikolp34@gmail.com>
2023-11-22 18:22:59 +0900

    feat: 커뮤니티 리스트 페이지네이션 기능 추가

--- src/components/Community/CommunityItem/CommunityPageList.tsx
+++ src/components/Community/CommunityItem/CommunityPageList.tsx
@@ -6,42 +6,55 @@ import { Community } from 'types/community.type';
 
 import { requestCommunityData } from 'apis/community';
 import { requestCommunityCount } from 'apis/communityCount';
+import MenuPagination from 'components/commons/Pagination/MenuPagination';
+import { useControlPageParam } from 'hooks/useControlPageParam';
 
 import CommunityItem from './CommunityItem';
 
 export const CommunityPageList = () => {
   const { menuName } = useParams();
   const [communityList, setCommunityList] = useState<Community[]>([]);
+  const [totalCount, setTotalCount] = useState(1);
+  const { getPageParam } = useControlPageParam();
+  const currentPage = getPageParam ? Number(getPageParam) : 1;
 
   useEffect(() => {
     (async () => {
       try {
-        const res = await requestCommunityData();
+        const res = await requestCommunityData(getPageParam as string);
         const totalCount = await requestCommunityCount();
         setCommunityList(res.data.data);
-        console.log(totalCount);
+        setTotalCount(totalCount.data.data);
       } catch (error) {
         console.error(error);
       }
     })();
-  }, [menuName]);
+  }, [menuName, getPageParam]);
   if (communityList)
     return (
-      <List>
-        {communityList.map(el => (
-          <CommunityItem
-            id={el.id}
-            key={el.id}
-            category={el.category}
-            title={el.title}
-            date={el.date}
-            view={el.view}
-            like={el.like}
-            comment={el.comment}
-            user={el.user}
-          />
-        ))}
-      </List>
+      <>
+        <List>
+          {communityList.map(el => (
+            <CommunityItem
+              id={el.id}
+              key={el.id}
+              category={el.category}
+              title={el.title}
+              date={el.date}
+              view={el.view}
+              like={el.like}
+              comment={el.comment}
+              user={el.user}
+            />
+          ))}
+        </List>
+
+        <MenuPagination
+          currentPage={currentPage}
+          totalItemsCount={totalCount}
+          pageItemsCount={communityList.length}
+        />
+      </>
     );
 };
 
