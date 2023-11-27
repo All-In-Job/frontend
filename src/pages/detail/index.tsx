5a9a0c86106d0d5157b572d6af048c1555b984e9
chlgusrb97 <ikolp34@gmail.com>
2023-11-28 04:40:38 +0900

    feat: 상세페이지 menuName 커뮤니티 제외 메뉴 api 호출 조건문 및 detailData 없을 시 커뮤니티 디테일 렌더링

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -27,10 +27,11 @@ export const DetailPage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await requestDetailCrawlingData(userId?.id, menuName, detailId);
-        if (res) {
-          setDetailData(res.data.data);
-          console.log(res);
+        if (menuName !== 'community') {
+          const res = await requestDetailCrawlingData(userId?.id, menuName, detailId);
+          if (res) {
+            setDetailData(res.data.data);
+          }
         }
       } catch (error) {
         console.error(error);
@@ -42,78 +43,74 @@ export const DetailPage = () => {
 
   console.log(detailData);
 
+  if (!detailData) return <CommunityDetail />;
+
   if (detailData)
     return (
       <>
-        {menuName !== 'community' ? (
-          <>
-            <DetailPageInfo
+        <DetailPageInfo
+          title={detailData.title}
+          dDay={12}
+          bookmarkCount={detailData.scrap}
+          viewCount={detailData.view}
+          id={detailId}
+          isScrap={detailData.isScrap}
+        >
+          {/* 상황에 맞는 컴포넌트 추가 */}
+          {'enterprise' in detailData && (
+            <RestDetailPageInfo
+              type={detailData.type}
+              menuName={menuName}
+              mainImage={detailData.mainImage}
+              enterprise={detailData.enterprise}
+              institution={detailData.institution}
+              target={detailData.target}
+              period={detailData.period}
+              participationPeriod={detailData.participationPeriod}
+              preferentialTreatment={detailData.preferentialTreatment}
+              scale={detailData.scale}
+              benefits={detailData.benefits}
+              interests={detailData.interests}
+              field={detailData.field}
+              homePage={detailData.homePage}
+              location={detailData.location}
+              personnel={detailData.personnel}
+            />
+          )}
+
+          {'enTitle' in detailData && (
+            <CertificateDetailInfo
+              type={detailData.type}
+              mainImage={detailData.mainImage}
               title={detailData.title}
-              dDay={12}
-              bookmarkCount={detailData.scrap}
-              viewCount={detailData.view}
-              id={detailId}
-              isScrap={detailData.isScrap}
-            >
-              {/* 상황에 맞는 컴포넌트 추가 */}
-              {'enterprise' in detailData && (
-                <RestDetailPageInfo
-                  type={detailData.type}
-                  menuName={menuName}
-                  mainImage={detailData.mainImage}
-                  enterprise={detailData.enterprise}
-                  institution={detailData.institution}
-                  target={detailData.target}
-                  period={detailData.period}
-                  participationPeriod={detailData.participationPeriod}
-                  preferentialTreatment={detailData.preferentialTreatment}
-                  scale={detailData.scale}
-                  benefits={detailData.benefits}
-                  interests={detailData.interests}
-                  field={detailData.field}
-                  homePage={detailData.homePage}
-                  location={detailData.location}
-                  personnel={detailData.personnel}
-                />
-              )}
+              enTitle={detailData.enTitle}
+              relateDepartment={detailData.relateDepartment}
+              institution={detailData.institution}
+            />
+          )}
+        </DetailPageInfo>
+        <S.Container>
+          <S.Title>{'enterprise' in detailData ? '상세내용' : '시험일정'}</S.Title>
 
-              {'enTitle' in detailData && (
-                <CertificateDetailInfo
-                  type={detailData.type}
-                  mainImage={detailData.mainImage}
-                  title={detailData.title}
-                  enTitle={detailData.enTitle}
-                  relateDepartment={detailData.relateDepartment}
-                  institution={detailData.institution}
-                />
-              )}
-            </DetailPageInfo>
-            <S.Container>
-              <S.Title>{'enterprise' in detailData ? '상세내용' : '시험일정'}</S.Title>
+          {/* 상황에 맞는 컴포넌트 추가 */}
+          {'enterprise' in detailData && (
+            <DetailDescription type={detailData.type} detail={detailData.detail} />
+          )}
 
-              {/* 상황에 맞는 컴포넌트 추가 */}
-              {'enterprise' in detailData && (
-                <DetailDescription type={detailData.type} detail={detailData.detail} />
-              )}
-
-              {'enTitle' in detailData && (
-                <CertificateExamSchedule
-                  id={pathProps(detailData.examSchedules)?.id}
-                  key={pathProps(detailData.examSchedules)?.id}
-                  turn={pathProps(detailData.examSchedules)?.turn}
-                  wtReceipt={pathProps(detailData.examSchedules)?.wtReceipt}
-                  wtDday={pathProps(detailData.examSchedules)?.wtDday}
-                  wtResultDay={pathProps(detailData.examSchedules)?.wtResultDay}
-                  ptReceipt={pathProps(detailData.examSchedules)?.ptReceipt}
-                  ptDday={pathProps(detailData.examSchedules)?.ptDday}
-                  resultDay={pathProps(detailData.examSchedules)?.resultDay}
-                />
-              )}
-            </S.Container>
-          </>
-        ) : (
-          <CommunityDetail />
-        )}
+          {'enTitle' in detailData && (
+            <CertificateExamSchedule
+              id={pathProps(detailData.examSchedules)?.id}
+              key={pathProps(detailData.examSchedules)?.id}
+              turn={pathProps(detailData.examSchedules)?.turn}
+              wtReceipt={pathProps(detailData.examSchedules)?.wtReceipt}
+              wtDday={pathProps(detailData.examSchedules)?.wtDday}
+              wtResultDay={pathProps(detailData.examSchedules)?.wtResultDay}
+              ptReceipt={pathProps(detailData.examSchedules)?.ptReceipt}
+              ptDday={pathProps(detailData.examSchedules)?.ptDday}
+              resultDay={pathProps(detailData.examSchedules)?.resultDay}
+            />
+          )}
+        </S.Container>
       </>
     );
 };
