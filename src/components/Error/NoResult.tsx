9bbe8fa880621c308e33747426176b7470e7b097
nearworld <artemismars2@outlook.com>
2023-11-28 18:41:25 +0900

    feat: 게시물 리스트가 없을때 보여주는 검색 결과 없음 컴포넌트

---
+++ src/components/Error/NoResult.tsx
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+
+import theme from 'styles/theme';
+
+export const NoResult = () => {
+  return (
+    <StyledContainer>
+      <StyledImageContainer>
+        <img alt='no result' src={'src/assets/no_result.svg'} width={244} height={207} />
+      </StyledImageContainer>
+      <StyledText>찾으시는 커리어에 대한 검색 결과가 없습니다.</StyledText>
+    </StyledContainer>
+  );
+};
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 437px;
+  height: 322px;
+  text-align: center;
+`;
+const StyledImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 250px;
+  height: 250px;
+  background-color: ${theme.palette.orange100};
+  border-radius: 20px;
+`;
+const StyledText = styled.p`
+  font-weight: 700;
+  margin-top: 40px;
+  font-size: 24px;
+`;
