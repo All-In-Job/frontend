dd6cc59b6bca5647bff90fa86ec3350699a4650e
chlgusrb97 <ikolp34@gmail.com>
2023-12-07 18:35:14 +0900

    design: KeywordButton 삭제 버튼 추가

--- src/components/MenuFilter/Buttons/KeywordButton.tsx
+++ src/components/MenuFilter/Buttons/KeywordButton.tsx
@@ -4,6 +4,8 @@ import styled from '@emotion/styled';
 
 import theme from 'styles/theme';
 
+import { ReactComponent as DeleteIcon } from '/src/components/MenuFilter/res/img/delete.svg';
+
 const { palette, textStyle } = theme;
 
 type Props = {
@@ -12,12 +14,18 @@ type Props = {
 };
 
 const KeywordButton: FC<Props> = ({ keyword, isActive }) => {
-  return <Button isActive={isActive}>#{keyword}</Button>;
+  return (
+    <Button isActive={isActive}>
+      #{keyword} <Delete />
+    </Button>
+  );
 };
 
 export default KeywordButton;
 
 const Button = styled.button<{ isActive: boolean }>`
+  display: flex;
+  align-items: center;
   padding: 6px 14px;
   border-radius: 999px;
   border: 2px solid ${props => (props.isActive ? palette.orange500 : palette.background.primary)};
@@ -26,4 +34,8 @@ const Button = styled.button<{ isActive: boolean }>`
   font-family: ${props => (props.isActive ? 'Bold' : 'Medium')};
   line-height: ${textStyle.body01.lineHeight};
   cursor: pointer;
+`;
+
+const Delete = styled(DeleteIcon)`
+  margin-left: 8px;
 `;
