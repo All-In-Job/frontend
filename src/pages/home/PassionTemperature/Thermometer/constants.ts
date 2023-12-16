327d243a6e3e84b3365b3807d61acb12f11d8035
hyundee <hyundee9@gmail.com>
2023-12-16 20:09:03 +0900

    fix : 타입 분리

--- src/pages/home/PassionTemperature/Thermometer/constants.ts
+++ src/pages/home/PassionTemperature/Thermometer/constants.ts
@@ -1,8 +1,8 @@
-import { BarPieceColor } from './types';
+import { temperatureColor } from './types';
 
 export const MAX_PERCENT = 20;
 
-export const BAR_PIECE_COLOR: BarPieceColor = {
+export const BAR_PIECE_COLOR: temperatureColor = {
   competition: '#FFC1BD',
   outside: '#FFE99B',
   qnet: '#A7F9B4',
--- src/pages/home/PassionTemperature/Thermometer/types.ts
+++ src/pages/home/PassionTemperature/Thermometer/types.ts
@@ -1,13 +1,20 @@
 export interface BarPieceProps {
-  barType: keyof BarPieceColor;
+  barType: keyof temperatureColor;
   percent: number;
 }
+
+export type temperatureColor = {
+  competition: string;
+  outside: string;
+  qnet: string;
+  language: string;
+  intern: string;
+};
 
 export type BarPieceColor = {
   competition: string;
   outside: string;
   qnet: string;
-  language: string;
   intern: string;
 };
 
@@ -19,4 +26,4 @@ export type BarPieceAttr = {
 
 export type Thermometer = Pick<BarPieceProps, 'barType' | 'percent'>;
 
-export type ThermometerList = Record<keyof BarPieceColor, Thermometer>;
+export type ThermometerList = Record<keyof temperatureColor, Thermometer>;
