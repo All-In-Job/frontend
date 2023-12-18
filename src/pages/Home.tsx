4660cbf4be9c9e4883dfca417d3589b6b1c6587b
nearworld <artemismars2@outlook.com>
2023-12-18 20:32:03 +0900

    fix: Aside 위치 조정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
-import { Outlet, useOutlet } from 'react-router-dom';
+import { useState } from 'react';
 
+import styled from '@emotion/styled';
+import { Outlet, useLoaderData, useOutlet } from 'react-router-dom';
+
+import Aside from 'components/Aside/Aside';
 import { Layout } from 'components/Layout/Layout';
 import { Main } from 'components/Main/Main';
 import Header from 'components/Navigation/Header/Header';
 
+import AsideProfile from './home/AsideProfile';
+
 export const Home = () => {
   const outlet = useOutlet();
+  const loader = useLoaderData();
+
+  const [marginTop, setMarginTop] = useState('');
 
   const isHeaderShown = () => {
     const { pathname } = location;
@@ -17,7 +26,10 @@ export const Home = () => {
   return (
     <>
       {isHeaderShown()}
-      <Layout>{outlet ? <Outlet /> : <Main />}</Layout>
+      <Layout setMarginTop={setMarginTop}>{outlet ? <Outlet /> : <Main />}</Layout>
+      <Sidebar style={{ marginTop }}>{loader ? <AsideProfile /> : <Aside />}</Sidebar>
     </>
   );
 };
+
+const Sidebar = styled.aside``;
