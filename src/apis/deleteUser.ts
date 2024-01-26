5030c6b8d0db93179543636b51d4e48e7bbcba4d
chlgusrb97 <ikolp34@gmail.com>
2024-01-26 20:59:46 +0900

    chore: 회원 탈퇴 코드 삭제

--- src/apis/deleteUser.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const deleteUser = async (email: string) => {
-  return await axios.delete(`${import.meta.env.VITE_API_BASE_URL}/user/delete`, {
-    data: {
-      email,
-    },
-  });
-};
